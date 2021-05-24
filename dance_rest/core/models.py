9e712e90ff08f4edf629023b7923b8300a720b01
alessandroatgmail <alessandro.silvestrin.as@gmail.com>
2021-05-24 15:34:32 +0200

    update for admin user support

--- dance_rest/core/models.py
+++ dance_rest/core/models.py
@@ -15,7 +15,7 @@ class UserManager(BaseUserManager):
         user = self.model(email=self.normalize_email(email),
                           **extra_fields)
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
@@ -24,7 +24,7 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save()
+        user.save(using=self._db)
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -33,7 +33,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     """
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
